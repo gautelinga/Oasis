4b19d58ebff5eeaf39d78e179cfbc4fe79734bbe
Gaute Linga <gaute.linga@gmail.com>
2018-02-07 14:24:12 +0100

    Some updates to mask puff away.

--- mask_puff_away.py
+++ mask_puff_away.py
@@ -9,6 +9,9 @@ parser.add_argument("folder", type=str, help="Folder")
 parser.add_argument("x_left", type=float, help="x_left")
 parser.add_argument("x_right", type=float, help="x_right")
 parser.add_argument("snapshot_id", type=int, help="Snapshot ID")
+parser.add_argument("--other_snapshot_id", default=None,
+                    type=int, help="Snapshot ID 2")
+parser.add_argument("--noshift", action="store_true", help="Don't shift")
 parser.add_argument("--invert", action="store_true", help="Invert mask")
 args = parser.parse_args()
 
@@ -34,6 +37,16 @@ geometry = np.array(h5fu_in["Mesh/0/mesh/geometry"])
 topology = np.array(h5fu_in["Mesh/0/mesh/topology"])
 u = np.array(h5fu_in["VisualisationVector/{}".format(snapshot_id)])
 p = np.array(h5fp_in["VisualisationVector/{}".format(snapshot_id)])
+
+if args.other_snapshot_id is not None:
+    u2 = np.array(h5fu_in["VisualisationVector/{}".format(
+        args.other_snapshot_id)])
+    p2 = np.array(h5fu_in["VisualisationVector/{}".format(
+        args.other_snapshot_id)])
+else:
+    u2 = u
+    p2 = p
+
 h5fu_in.close()
 h5fp_in.close()
 
@@ -49,10 +62,18 @@ pts_high = geometry[num_edge+num_unique/2:, :]
 pts_high - pts_low
 assert(np.max(pts_high - pts_low, 0) == [0., 0., 40.])
 
-u_shift = np.zeros_like(u)
-u_shift[num_edge:num_edge+num_unique/2, :] = u[num_edge+num_unique/2:, :]
-u_shift[num_edge+num_unique/2:, :] = u[num_edge:num_edge+num_unique/2, :]
-u_shift[:num_edge, :] = u[:num_edge, :]
+u_shift = np.zeros_like(u2)
+p_shift = np.zeros_like(p2)
+if args.noshift:
+    u_shift[:, :] = u2[:, :]
+    p_shift[:, :] = p2[:, :]
+else:
+    u_shift[num_edge:num_edge+num_unique/2, :] = u2[num_edge+num_unique/2:, :]
+    u_shift[num_edge+num_unique/2:, :] = u2[num_edge:num_edge+num_unique/2, :]
+    u_shift[:num_edge, :] = u2[:num_edge, :]
+    p_shift[num_edge:num_edge+num_unique/2, :] = p2[num_edge+num_unique/2:, :]
+    p_shift[num_edge+num_unique/2:, :] = p2[num_edge:num_edge+num_unique/2, :]
+    p_shift[:num_edge, :] = p2[:num_edge, :]
 
 mask = 0.5*(np.tanh(geometry[:, 2]-x_left)-np.tanh(geometry[:, 2]-x_right))
 if args.invert:
@@ -62,11 +83,6 @@ u_out = np.zeros_like(u)
 u_out[:, 0] = (1.-mask)*u[:, 0] + mask*u_shift[:, 0]
 u_out[:, 1] = (1.-mask)*u[:, 1] + mask*u_shift[:, 1]
 u_out[:, 2] = (1.-mask)*u[:, 2] + mask*u_shift[:, 2]
-
-p_shift = np.zeros_like(p)
-p_shift[num_edge:num_edge+num_unique/2, :] = p[num_edge+num_unique/2:, :]
-p_shift[num_edge+num_unique/2:, :] = p[num_edge:num_edge+num_unique/2, :]
-p_shift[:num_edge, :] = p[:num_edge, :]
 
 p_out = np.zeros_like(p)
 p_out[:, 0] = (1.-mask)*p[:, 0] + mask*p_shift[:, 0]
